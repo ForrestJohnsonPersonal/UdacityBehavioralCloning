8d20efb0168698a0fb2f3f033d21972a12c139eb
Forrest Johnson <FJohns@polarisind.com>
2021-08-28 09:40:42 -0500

    Deleted previous attempts and added latest

--- model_Nvidia_MultiCam_AugGen2.py
+++ model_Nvidia_MultiCam_AugGen2.py
@@ -41,7 +41,7 @@ def process_image(img):
  #   image = np.array(img[80:140,:]) #img[60:135, :, :]
  #   image = cv2.resize(image, (image_W, image_H), cv2.INTER_AREA)
     image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    image = cv2.GaussianBlur(image, (5,5), 0)
+    image = cv2.GaussianBlur(image, (3,3), 0)
     return image
 
 def Get3CarCameraImages(path, steering_center, HardBias=False, steering_HardBias=0):
@@ -55,8 +55,8 @@ def Get3CarCameraImages(path, steering_center, HardBias=False, steering_HardBias
     steering_right = steering_center - correction
     if (HardBias == True):
         steering_center = steering_center + steering_HardBias
-        steering_left = steering_center - steering_HardBias/1.5
-        steering_right = steering_right + steering_HardBias/1.5
+        steering_left = steering_center - steering_HardBias/2
+        steering_right = steering_center + steering_HardBias/2
     imagesCenter.append(np.asarray(process_image(cv2.imread(row[0])))) #Image.open(row[0]))))
     measurementsCenter.append(steering_center)
     imagesLeft.append(np.asarray(process_image(cv2.imread(row[1])))) #Image.open(row[1]))))
@@ -93,44 +93,70 @@ with open('data1/dataFullLoop/driving_log.csv') as csvfile:
         # read in images from center, left and right cameras
 #        Get3CarCameraImages('data1/dataFullOptionalLoop/IMG/', float(row[3]))
 #Grab the Bridge data.        
-with open('data1/dataBridge1/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader) #skips the first line
-    for row in reader:
+#with open('data1/dataBridge1/driving_log.csv') as csvfile:
+#    reader = csv.reader(csvfile)
+#    next(reader) #skips the first line
+#    for row in reader:
         # read in images from center, left and right cameras
-        Get3CarCameraImages('data1/dataBridge1/IMG/', float(row[3]))     
+#        Get3CarCameraImages('data1/dataBridge1/IMG/', float(row[3]))     
 #Grab the AvoidBridgeSide data.        
-with open('data1/dataAvoidBridgeSideL/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader) #skips the first line
-    for row in reader:
+#with open('data1/dataAvoidBridgeSideL/driving_log.csv') as csvfile:
+#    reader = csv.reader(csvfile)
+#    next(reader) #skips the first line
+#    for row in reader:
         # read in images from center, left and right cameras
-        Get3CarCameraImages('data1/dataAvoidBridgeSideL/IMG/', float(row[3]), True, 0.6)# increase as we are doing the harder correction data below.
+#        Get3CarCameraImages('data1/dataAvoidBridgeSideL/IMG/', float(row[3]), True, 0.6)# increase as we are doing the harder correction data below.
 #with open('data1/dataAvoidBridgeSide/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader) #skips the first line
-    for row in reader:
+#    reader = csv.reader(csvfile)
+#    next(reader) #skips the first line
+#    for row in reader:
         # read in images from center, left and right cameras
-        Get3CarCameraImages('data1/dataAvoidBridgeSide/IMG/', float(row[3]), True, -0.6)# increase as we are doing the harder correction data below.          
+#        Get3CarCameraImages('data1/dataAvoidBridgeSide/IMG/', float(row[3]), True, -0.6)# increase as we are doing the harder correction data below.          
 #Grab the AvoidOffRoad data.        
-with open('data1/dataAvoidOffLeft/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader) #skips the first line
-    for row in reader:
+#with open('data1/dataAvoidOffLeft/driving_log.csv') as csvfile:
+#    reader = csv.reader(csvfile)
+#    next(reader) #skips the first line
+#    for row in reader:
         # read in images from center, left and right cameras
-        Get3CarCameraImages('data1/dataAvoidOffLeft/IMG/', float(row[3]), True, 0.6)# increase as we are doing the harder correction data below.
-with open('data1/dataAvoidOffRight/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader) #skips the first line
-    for row in reader:
+#        Get3CarCameraImages('data1/dataAvoidOffLeft/IMG/', float(row[3]), True, 0.6)# increase as we are doing the harder correction data below.
+#with open('data1/dataAvoidOffRight/driving_log.csv') as csvfile:
+#    reader = csv.reader(csvfile)
+#    next(reader) #skips the first line
+#    for row in reader:
         # read in images from center, left and right cameras
-        Get3CarCameraImages('data1/dataAvoidOffRight/IMG/', float(row[3]), True, -0.6)# increase as we are doing the harder correction data below.        
+#        Get3CarCameraImages('data1/dataAvoidOffRight/IMG/', float(row[3]), True, -0.6)# increase as we are doing the harder correction data below.        
 
-X_train = np.concatenate((np.array(imagesCenter), np.array(imagesLeft), np.array(imagesRight)), axis=0)
-y_train = np.concatenate((np.array(measurementsCenter), np.array(measurementsLeft), np.array(measurementsRight)), axis=0)
+X_train = []
+y_train = []
 #Get normal plus left/right flipped data.
-X_train = np.concatenate((X_train, np.fliplr(X_train)), axis=0)
-y_train = np.concatenate((y_train, np.negative(y_train)), axis=0)
+for offset in range(0, len(imagesCenter)):
+    imgCenter = np.array(imagesCenter[offset])
+    imgLeft = np.array(imagesLeft[offset])
+    imgRight = np.array(imagesRight[offset])
+    imgCenterFlip = np.fliplr(imgCenter)
+    imgLeftFlip = np.fliplr(imgLeft)
+    imgRightFlip = np.fliplr(imgRight)
+    measCenter = np.array(measurementsCenter[offset])
+    measLeft = np.array(measurementsLeft[offset])
+    measRight = np.array(measurementsRight[offset])
+    measCenterFlip = -measCenter
+    measLeftFlip = -measLeft
+    measRightFlip = -measRight
+    X_train.append(imgCenter)
+    X_train.append(imgLeft)
+    X_train.append(imgRight)
+    X_train.append(imgCenterFlip)
+    X_train.append(imgLeftFlip)
+    X_train.append(imgRightFlip)
+    y_train.append(measCenter)
+    y_train.append(measLeft)
+    y_train.append(measRight)
+    y_train.append(measCenterFlip)
+    y_train.append(measLeftFlip)
+    y_train.append(measRightFlip)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 print (X_train.shape)
 
 batch_size=64
@@ -156,15 +182,15 @@ model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Conv2D(filters=24,kernel_size=5,strides=2,activation='relu', kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01)))
-model.add(Conv2D(filters=36,kernel_size=5,strides=2,activation="relu", kernel_regularizer=regularizers.l2(0.01),
-                activity_regularizer=regularizers.l1(0.01)))
-model.add(Conv2D(filters=48,kernel_size=5,strides=2,activation="relu", kernel_regularizer=regularizers.l2(0.01),
-                activity_regularizer=regularizers.l1(0.01)))
+#model.add(Conv2D(filters=36,kernel_size=5,strides=2,activation="relu", kernel_regularizer=regularizers.l2(0.01),
+#                activity_regularizer=regularizers.l1(0.01)))
+#model.add(Conv2D(filters=48,kernel_size=5,strides=2,activation="relu", kernel_regularizer=regularizers.l2(0.01),
+#                activity_regularizer=regularizers.l1(0.01)))
+#model.add(Convolution2D(64,3,3, activation="relu", kernel_regularizer=regularizers.l2(0.01),
+#                activity_regularizer=regularizers.l1(0.01)))
 model.add(Convolution2D(64,3,3, activation="relu", kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01)))
-model.add(Convolution2D(64,3,3, activation="relu", kernel_regularizer=regularizers.l2(0.01),
-                activity_regularizer=regularizers.l1(0.01)))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100, kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01)))
@@ -207,7 +233,7 @@ history_object = model.fit_generator(train_generator, steps_per_epoch=math.ceil(
 #          steps_per_epoch=steps_per_epoch,
 #          validation_steps=validation_steps)
 
-model.save('model_Nvidia_MultiCam_AugGenerator5.h5')
+model.save('model_Nvidia_MultiCam_AugGenerator7.h5')
 
 ### print the keys contained in the history object
 print(history_object.history.keys())
